73f4e083ff142db33ca5289c975b25fda90ac72f
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-24 13:42:45 -0700

    Funcionamiento de api

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const ghibil ={
     name : document.getElementById(`ghibil-name`),
     sprite : document.getElementById(`ghibil-sprite`),
     descripcion: document.getElementById(`ghibil-descripcion`)
+   
 };
 
-const apiUrl ="https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49";
+const apiUrl ="https://ghibliapi.herokuapp.com/films/";
 
 const getGhibilDescripcionData = async url =>{
     const response = await fetch(url);
     const ghibilData = await response.json();
     console.log(ghibilData);
-}
+    return ghibilData;
+};
 
+let getGhibilData = async () =>{
+    const response = await fetch (`${apiUrl}58611129-2dbc-4a81-a72f-77ddfc1b1b49`)
+
+
+};
 getGhibilDescripcionData(apiUrl);
