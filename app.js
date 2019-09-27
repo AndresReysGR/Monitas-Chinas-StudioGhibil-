52a9858adde492e4ec919f34581622ca99f2b47e
rech99 <rech_99@hotmail.com>
2019-09-26 21:51:00 -0700

    cambios

--- app.js
+++ app.js
@@ -3,75 +3,49 @@
  /*hay como 3 errores en el sitio, sin embargo, son por la Api de la misma pagina, asi que no hay problema*/  
 /*Todo este codigo es la Api, hace falta hacer el html, para que se imprima la informacion en el sitio*/
 
-const apiUrl ="https://ghibliapi.herokuapp.com/films/"; 
+
+const title = document.getElementById('title');
+const release_date = document.getElementById('release_date');
+const director = document.getElementById('director');
+const producer = document.getElementById('producer');
+const description = document.getElementById('description');
+
+const url ="https://ghibliapi.herokuapp.com/films/"; 
  /* Parte de la Api que dice las localizaciones*/
-const getLocations = async url =>{
-    const response = await fetch(url);
-    const dataLocations = await response.json();
-    //console.log(dataLocations);
-    return dataLocations;
-}
-/* Parte de la Api que dice las personas que participaron*/
-const getPeople = async url =>{
-    const response = await fetch(url);
-    const dataPeople = await response.json();
-    //console.log(dataPeople);
-    return dataPeople;
-}
+
 /*Api Principal*/
-const getGhibilDescripcionData = async url =>{
+const getGhibliDescripcionData = async url =>{
     const response = await fetch(url);
-    const ghibilData = await response.json();
+    const ghibliData = await response.json();
   // console.log(ghibilData);
-    ghibilData.forEach(async element => {
+
+  
+    ghibliData.forEach(async element => {
         /*aqui vienen todos los datos de la Api*/
-        const {title, director, description, people, producer, locations, release_date} = element;
+        const {title, director, description, producer, release_date} = element;
 
         console.log(title);
         console.log(director);
         console.log(producer);
         console.log(description);
         console.log(release_date)
-        //console.log(people);
-        //console.log(locations);
+      
 
-      /*Parte de la Api de loacalizaciones que se encuentra al principio del codigo*/
-        getLocations(locations).then(
-            dataLocations =>{
-                dataLocations.forEach(element => {
-                    const {climate, name, tarrain} = element;
-                   console.log(climate);
-                   console.log(name);
-                    console.log(tarrain);
-                
-                });
-            }
-        );
         
-        /*Parte de la Api de loacalizaciones que se encuentra al principio del codigo*/
-        getPeople(people).then(
-            dataPeople =>{
-                dataPeople.forEach(element => {
-                    const {name, age, gender, eye_color, } = element;
-                   console.log(name);
-                   console.log(age);
-                console.log(gender);
-                    //console.log(eye_color);
-                });
-            }
-        );
-           
-       
-        
-       
+    title.innerText = title;
+    director.innerText = director;
+    producer.innerText = producer;
+    release_date.innerText = release_date;
+    description.innerText = description;
+     
         });
-    return ghibilData;
+    return ghibliData
 
-    
+  
    
 };
 
 /*let getGhibilData = async () =>{
-    const response = await fetch (`${apiUrl}`)
-};*/
-getGhibilDescripcionData(apiUrl);
+    const response = await fetch (`${url}`)
+}*/
+getGhibliDescripcionData(url);
